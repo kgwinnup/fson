3b3d7c277e6d9c4fa69c50435a18cb7bfb875c65
kgwinnup <kgwinnup@carbonblack.com>
2019-09-23 14:01:30 -0400

    added main and readme

--- fson.go
+++ fson.go
@@ -163,5 +163,34 @@ func (self *Fson) Fmap(f fmapFn) {
 }
 
 func main() {
+	data := []byte("{\"foo\": 1, \"foo2\": {\"bar\": 1, \"baz\": [1,1,1]}}")
+
+	fsonobj := New()
+	if err := fsonobj.Loads(&data); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(fsonobj)
+	fsonobj.Fmap(func(v interface{}) interface{} {
+		switch v.(type) {
+		case float64:
+			return v.(float64) + 1
+		default:
+			return v
+		}
+	})
+	fmt.Println(fsonobj)
+
+	data = []byte("{\"boo\": true, \"hello\": \"world\", \"obj\": {\"foo\": \"bar\"}, \"baz\": [400,2,3]}")
+	if err := fsonobj.Loads(&data); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsonobj)
+
+	fsonobj.Insert([]string{"new", "field"}, "hello world", false)
+	fmt.Println(fsonobj)
+
+	newfield, _ := fsonobj.Get([]string{"new", "field"})
+	fmt.Println(newfield.(string))
 
 }

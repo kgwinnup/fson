e150710eaa9fd0a58da51a4585d0d140f1bb731d
Kyle Gwinnup <kylegwinnup@kpgair.local>
2019-11-16 21:19:09 -0500

    added more comments and minor code cleanup

--- fson.go
+++ fson.go
@@ -14,29 +14,36 @@ type Fson struct {
 	Data map[string]interface{}
 }
 
-func (self *Fson) MarshalJSON() ([]byte, error) {
-	return json.Marshal(self.Data)
+// MarshalJSON implementation for JSON encoding
+func (f *Fson) MarshalJSON() ([]byte, error) {
+	return json.Marshal(f.Data)
 }
 
-func (self *Fson) UnmarshalJSON(b []byte) error {
-	return self.Loads(b)
+// UnmarshalJSON impelmentation for JSON decoding to Fson
+func (f *Fson) UnmarshalJSON(b []byte) error {
+	return f.Loads(b)
 }
 
-func (self *Fson) Scan(src interface{}) error {
-	return self.Loads(src.([]byte))
+// Scan will take a pointer interface and attempt to decode as Fson structure
+func (f *Fson) Scan(src interface{}) error {
+	return f.Loads(src.([]byte))
 }
 
-func (self Fson) Bytes() []byte {
-	if b, err := json.Marshal(self.Data); err == nil {
+// Bytes will return a byte slice of the underlying JSON data. If there is an
+// error an empty byte slice is returned
+func (f *Fson) Bytes() []byte {
+	if b, err := json.Marshal(f.Data); err == nil {
 		return b
 	}
 	return []byte{}
 }
 
-func (self Fson) String() string {
-	return string(self.Bytes())
+// String converts the Fson object into a string
+func (f *Fson) String() string {
+	return string(f.Bytes())
 }
 
+// New initializes a new Fson object given a JSON byte slice
 func New(b []byte) *Fson {
 	if b == nil {
 		return &Fson{make(map[string]interface{})}
@@ -50,12 +57,12 @@ func New(b []byte) *Fson {
 // Loads will take nil or a []byte array and create a Fson object with it.
 // Generally nil is used if you want to build your JSON object from scratch with the Set method
 // This will overwrite any current data that is in the Fson object
-func (self *Fson) Loads(b []byte) error {
-	self.Data = make(map[string]interface{})
+func (f *Fson) Loads(b []byte) error {
+	f.Data = make(map[string]interface{})
 	if b == nil {
 		return nil
 	}
-	if err := json.Unmarshal(b, &self.Data); err != nil {
+	if err := json.Unmarshal(b, &f.Data); err != nil {
 		return err
 	}
 	return nil
@@ -63,27 +70,28 @@ func (self *Fson) Loads(b []byte) error {
 
 // FromFile will load a JSON object from a file
 func FromFile(path string) (*Fson, error) {
-	if data, err := ioutil.ReadFile("/tmp/dat"); err != nil {
+	data, err := ioutil.ReadFile("/tmp/dat")
+	if err != nil {
 		return New(data), nil
-	} else {
-		return nil, err
 	}
+
+	return nil, err
 }
 
 // ParseJSON takes some bytes and parses it into an Fson object
 func ParseJSON(b []byte) (*Fson, error) {
 	if b == nil {
 		return nil, fmt.Errorf("no bytes provided to parse as JSON")
-	} else {
-		f := &Fson{}
-		if err := json.Unmarshal(b, &f.Data); err != nil {
-			return nil, err
-		}
-		return f, nil
 	}
+
+	f := &Fson{}
+	if err := json.Unmarshal(b, &f.Data); err != nil {
+		return nil, err
+	}
+	return f, nil
 }
 
-func (self *Fson) set(path []string, value interface{}, cur map[string]interface{}, appendList bool) {
+func (f *Fson) set(path []string, value interface{}, cur map[string]interface{}, appendList bool) {
 	// check if we are where the insert should happen
 	if len(path) == 1 {
 		if appendList {
@@ -108,24 +116,24 @@ func (self *Fson) set(path []string, value interface{}, cur map[string]interface
 		if _, ok := cur[path[0]]; !ok {
 			cur[path[0]] = make(map[string]interface{})
 		}
-		self.set(path[1:], value, cur[path[0]].(map[string]interface{}), appendList)
+		f.set(path[1:], value, cur[path[0]].(map[string]interface{}), appendList)
 	}
 }
 
 // Set will set a value to a specified path. The path is defined as a variadic
 // parameter. This will overwrite any value that is located at the provided key
-func (self *Fson) Set(value interface{}, path ...string) {
-	self.set(path, value, self.Data, false)
+func (f *Fson) Set(value interface{}, path ...string) {
+	f.set(path, value, f.Data, false)
 }
 
 // SetA is the same as set except that it will append to a list. If no list
 // exists, it will create a list. If an element currently exists, a new list
 // will be created with the existing element as the head of the list.
-func (self *Fson) SetA(value interface{}, path ...string) {
-	self.set(path, value, self.Data, true)
+func (f *Fson) SetA(value interface{}, path ...string) {
+	f.set(path, value, f.Data, true)
 }
 
-func (self *Fson) get(path []string, cur map[string]interface{}) interface{} {
+func (f *Fson) get(path []string, cur map[string]interface{}) interface{} {
 	if len(path) == 1 {
 		if _, ok := cur[path[0]]; ok {
 			return cur[path[0]]
@@ -135,53 +143,58 @@ func (self *Fson) get(path []string, cur map[string]interface{}) interface{} {
 	}
 
 	if _, ok := cur[path[0]].(map[string]interface{}); ok {
-		return self.get(path[1:], cur[path[0]].(map[string]interface{}))
-	} else {
-		return nil
+		return f.get(path[1:], cur[path[0]].(map[string]interface{}))
 	}
+
+	return nil
 }
 
 // Get works like set in that the path to the key is specified as a list of
 // string values which represent the orderd nested object keys
-func (self *Fson) Get(path ...string) (interface{}, bool) {
+func (f *Fson) Get(path ...string) (interface{}, bool) {
 	if len(path) == 0 {
 		return nil, false
 	}
 
-	if v := self.get(path, self.Data); v == nil {
+	v := f.get(path, f.Data)
+	if v == nil {
 		return nil, false
-	} else {
-		return v, true
 	}
+
+	return v, true
 }
 
 // GetObject will work the same as Get except this will automatically encode the
 // result into an Fson object. If the value retrieved is not a JSON object,
 // this will return nil, false
-func (self *Fson) GetObject(path ...string) (*Fson, bool) {
+func (f *Fson) GetObject(path ...string) (*Fson, bool) {
 	if len(path) == 0 {
 		return nil, false
 	}
 
-	if v := self.get(path, self.Data); v == nil {
+	v := f.get(path, f.Data)
+	if v == nil {
 		return nil, false
-	} else {
-		if b, err := json.Marshal(v); err != nil {
-			return nil, false
-		} else {
-			return New(b), true
-		}
 	}
+
+	b, err := json.Marshal(v)
+	if err != nil {
+		return nil, false
+	}
+
+	return New(b), true
 }
 
 // Exists will return true if the key exists in the JSON
-func (self *Fson) Exists(path ...string) bool {
-	_, ok := self.Get(path...)
+func (f *Fson) Exists(path ...string) bool {
+	_, ok := f.Get(path...)
 	return ok
 }
 
-func (self *Fson) GetArray(path ...string) ([]interface{}, error) {
-	data, ok := self.Get(path...)
+// GetArray will retrieve an array item from a specific key in the Fson object.
+// The JSON array will be returned as a slice of interface{}
+func (f *Fson) GetArray(path ...string) ([]interface{}, error) {
+	data, ok := f.Get(path...)
 	if !ok {
 		return nil, fmt.Errorf("key does not exist")
 	}
@@ -198,12 +211,12 @@ func (self *Fson) GetArray(path ...string) ([]interface{}, error) {
 // this value. This will change the JSON structure
 type FilterFn func(interface{}) bool
 
-func (self *Fson) filter(f FilterFn, value interface{}) interface{} {
+func (f *Fson) filter(fn FilterFn, value interface{}) interface{} {
 	switch value.(type) {
 	case []interface{}:
 		lst := make([]interface{}, 0, 0)
 		for _, item := range value.([]interface{}) {
-			if f(item) {
+			if fn(item) {
 				lst = append(lst, item)
 			}
 		}
@@ -211,8 +224,8 @@ func (self *Fson) filter(f FilterFn, value interface{}) interface{} {
 	case map[string]interface{}:
 		mp := make(map[string]interface{})
 		for k, val := range value.(map[string]interface{}) {
-			if f(val) {
-				mp[k] = self.filter(f, val)
+			if fn(val) {
+				mp[k] = f.filter(fn, val)
 			}
 		}
 		return mp
@@ -221,50 +234,57 @@ func (self *Fson) filter(f FilterFn, value interface{}) interface{} {
 	}
 }
 
-// Filter will filter out values from the JSON where the f "filterFn" returns false for that value
-func (self *Fson) Filter(f FilterFn) {
+// Filter will filter out values from the JSON where the f "filterFn" returns
+// false for that value
+func (f *Fson) Filter(fn FilterFn) {
 	mp := make(map[string]interface{})
-	for k, v := range self.Data {
-		if f(v) {
-			mp[k] = self.filter(f, v)
+
+	for k, v := range f.Data {
+		if fn(v) {
+			mp[k] = f.filter(fn, v)
 		}
 	}
-	self.Data = mp
+
+	f.Data = mp
 }
 
-// FmapFn will transform a value within the JSON into a new value, leaving the JSON structure alone
+// FmapFn will transform a value within the JSON into a new value, leaving the
+// JSON structure alone
 type FmapFn func(interface{}) interface{}
 
-func (self *Fson) fmap(f FmapFn, value interface{}) interface{} {
+func (f *Fson) fmap(fn FmapFn, value interface{}) interface{} {
 	switch value.(type) {
 	case []interface{}:
 		lst := make([]interface{}, 0, 0)
 		for _, item := range value.([]interface{}) {
-			item = self.fmap(f, item)
+			item = f.fmap(fn, item)
 			lst = append(lst, item)
 		}
 		return lst
 	case map[string]interface{}:
 		mp := make(map[string]interface{})
 		for k, val := range value.(map[string]interface{}) {
-			mp[k] = self.fmap(f, val)
+			mp[k] = f.fmap(fn, val)
 		}
 		return mp
 	default:
-		return f(value)
+		return fn(value)
 	}
 }
 
-// Fmap applys a function to every value in the JSON structure, mutating them in place
-func (self *Fson) Fmap(f FmapFn) {
+// Fmap applys a function to every value in the JSON structure, mutating them in
+// place
+func (f *Fson) Fmap(fn FmapFn) {
 	mp := make(map[string]interface{})
-	for k, v := range self.Data {
-		mp[k] = self.fmap(f, v)
+
+	for k, v := range f.Data {
+		mp[k] = f.fmap(fn, v)
 	}
-	self.Data = mp
+
+	f.Data = mp
 }
 
-func (self *Fson) del(path []string, cur interface{}) interface{} {
+func (f *Fson) del(path []string, cur interface{}) interface{} {
 	switch cur.(type) {
 	case map[string]interface{}:
 		mp := make(map[string]interface{})
@@ -276,7 +296,7 @@ func (self *Fson) del(path []string, cur interface{}) interface{} {
 			}
 		} else {
 			for key, val := range cur.(map[string]interface{}) {
-				mp[key] = self.del(path[1:], val)
+				mp[key] = f.del(path[1:], val)
 			}
 		}
 		return mp
@@ -286,56 +306,57 @@ func (self *Fson) del(path []string, cur interface{}) interface{} {
 }
 
 // Del will delete a key from the JSON object
-func (self *Fson) Del(path []string) {
+func (f *Fson) Del(path []string) {
 	mp := make(map[string]interface{})
-	for k, v := range self.Data {
+
+	for k, v := range f.Data {
 		if len(path) == 1 {
 			if k != path[0] {
 				mp[k] = v
 			}
 		} else {
 			if k == path[0] {
-				mp[k] = self.del(path[1:], v)
+				mp[k] = f.del(path[1:], v)
 			} else {
 				mp[k] = v
 			}
 		}
 	}
-	self.Data = mp
 
+	f.Data = mp
 }
 
-func (self *Fson) merge(path []string, obj interface{}) {
+func (f *Fson) merge(path []string, obj interface{}) {
 	switch obj.(type) {
 	case map[string]interface{}:
 		for k, v := range obj.(map[string]interface{}) {
 			newpath := append(path, k)
-			if _, ok := self.Get(newpath...); ok {
-				self.merge(newpath, v)
+			if _, ok := f.Get(newpath...); ok {
+				f.merge(newpath, v)
 			} else {
-				self.Set(v, newpath...)
+				f.Set(v, newpath...)
 			}
 		}
 	default:
-		self.Set(obj, path...)
+		f.Set(obj, path...)
 	}
 }
 
 // Merge will take two an Fson object and merge it with an existing Fson
 // object. The new object will overwrite any matching key values
-func (self *Fson) Merge(obj *Fson) {
+func (f *Fson) Merge(obj *Fson) {
 	for k, v := range obj.Data {
-		if _, ok := self.Data[k]; ok {
-			self.merge([]string{k}, v)
+		if _, ok := f.Data[k]; ok {
+			f.merge([]string{k}, v)
 		} else {
-			self.Data[k] = v
+			f.Data[k] = v
 		}
 	}
 }
 
 // Pretty returns a prety printed string of the underlying JSON
-func (self *Fson) Pretty() string {
+func (f *Fson) Pretty() string {
 	var out bytes.Buffer
-	json.Indent(&out, self.Bytes(), "", "    ")
+	json.Indent(&out, f.Bytes(), "", "    ")
 	return out.String()
 }
